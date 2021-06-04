be6c14c4df09c2cead187354125e5584c3aa4904
Greggoldman11 <Gregory.goldman97@gmail.com>
2021-06-04 11:55:28 -0400

    Dev (#2)
    * Create the model
    
    This commit contains the completed rating model for version 1 of the
    ratings app.  It contains all of the data fields, the owner, and the
    timestamps virtual
    
    * New commit for rating model
    
    The previous commit did not contain the intended resources, this is The
    commit that does contain it
    
    * Create routes and curl scripts
    
    This commit creates the router for ratings.  It also contains curl
    scripts for testing.  The router allows the user to make requests to
    create, show, update, or destroy their entries into the database. The
    only user who can make changes on their entries is the actual owner of
    that file.
    
    * Modify the curl script for index
    
    This includes a modifcation to the index.sh file
    
    * Changes the status and data
    
    This sends back a status code and body when updated

---
+++ app/routes/rating_routes.js
@@ -0,0 +1,74 @@
+// require express so we can make routes
+const express = require('express')
+
+// require passport for authentication
+const passport = require('passport')
+
+// require the model file
+const Rating = require('./../models/rating')
+
+// require the error handler
+const {
+  requireOwnership,
+  handle404,
+  BadParamsError,
+  BadCredentialsError
+}  = require('./../../lib/custom_errors')
+
+// require middleware
+const removeBlanks = require('./../../lib/remove_blank_fields')
+
+// require authentication with passort.authenticat(<strategy>, { session: false })
+const requireToken = passport.authenticate('bearer', { session: false })
+
+// create a router
+const router = express.Router()
+
+// create routes
+router.post('/ratings', requireToken, (req, res, next) => {
+  // I want to make the owner the current user
+  // by setting the owner as the user id
+  req.body.rating.owner = req.user.id
+
+  // create a new rating
+  Rating.create(req.body.rating)
+  // after I create my rating I want to display a 201 created and the body
+    .then(rating => {
+      res.status(201).json({ rating: rating })
+    })
+    .catch(next)
+})
+router.get('/ratings', requireToken, (req, res, next) => {
+  // locate all of my ratings
+  Rating.find()
+    .then(ratings => res.status(200).json({ ratings }))
+    .catch(next)
+})
+
+router.patch('/ratings:id', requireToken, removeBlanks, (req, res, next) => {
+  delete req.body.rating.owner
+
+  Rating.findById(req.params.id)
+
+    .then(handle404)
+    .then(rating => {
+      requireOwnership(req, rating)
+      return rating.updateOne(req.body.rating)
+    })
+    .then(rating => res.status(200).json({ rating }))
+    // .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+router.delete('/ratings:id', requireToken, (req, res, next) => {
+  Rating.findById(req.params.id)
+    .then(handle404)
+    .then(rating => {
+      requireOwnership(req, rating)
+      return rating.deleteOne(req.body.rating)
+    })
+    .then(() => res.sendStatus(204))
+    .catch(next)
+})
+
+module.exports = router
